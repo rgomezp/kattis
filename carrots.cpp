480fdab7d27de419c5e9cc3c1ddb9383d30e8cc2
Rodrigo Gomez Palacio <rogomezpalacio@gmail.com>
2017-11-14 01:21:33 -0600

    Finished carrots, flexible

---
+++ carrots.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+  int n,p;
+  scanf("%i %i", &n, &p);
+  for(int i=0; i<n; i++){
+    string temp;
+    cin >> temp;
+  }
+  cout<<p;
+  return 0;
+}
